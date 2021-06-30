cc54b4e28c92f21e78558a14f6e6edb58e4de548
Arman <armanexplorer@gmail.com>
2021-06-30 22:06:43 +0430

    some commenting

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.preprocessing import normalize, StandardScaler, OrdinalEncoder, OneHotEncoder, KBinsDiscretizer
 
@@ -25,11 +23,25 @@ Q2: Comment this line for applying discretizing and scaling
 """
 scaled_features = features
 
-agg_cluster = AgglomerativeClustering(n_clusters=6, affinity='euclidean', linkage='ward')  
+"""
+* ‘ward’ minimizes the variance of the clusters being merged.
+* If linkage is “ward”, only “euclidean” is accepted.
+"""
+agg_cluster = AgglomerativeClustering(n_clusters=6, affinity='euclidean', linkage='ward') 
+ 
+"""
+* ‘random’: choose n_clusters observations (rows) at random from data for the initial centroids.
+* ‘n_init’: Number of time the k-means algorithm will be run with different centroid seeds.
+* The final results will be the best output of n_init consecutive runs in terms of inertia.
+* ‘max_iter’: Maximum number of iterations of the k-means algorithm for a single run.
+* ‘random_state’ :Determines random number generation for centroid initialization
+"""
 kmean_cluster = KMeans(init="random", n_clusters=6, n_init=10, max_iter=100, random_state=261)
 
 agg_pred = agg_cluster.fit_predict(scaled_features)
 kmean_pred = kmean_cluster.fit_predict(scaled_features)
+
+# Accuracy
 agg_sum = 0
 kmean_sum = 0
 for i in range(len(y)):
@@ -37,8 +49,3 @@ for i in range(len(y)):
     kmean_sum += (kmean_pred[i] == y[i])
     
 print(f'Kmean:{kmean_sum/len(kmean_pred)} \nAgg: {agg_sum/len(agg_pred)}')
-
-
-
-
-# data_scaled = normalize(features)
